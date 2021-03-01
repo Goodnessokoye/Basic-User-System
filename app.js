ac6cf27122b8b2581332afa0dff0ed263a7c6166
Goodness <goodnessokoye10@gmail.com>
2021-03-01 22:26:35 +0100

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+const mongoose = require("mongoose")
+const express = require("express") 
+
+const app = require("express")()
+
+app.use(express.json());
 
 
+mongoose.connect("mongodb://localhost/UserSystem", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("connected to mongodb..."))
+  .catch((err) => console.error("could not connect to mongodb...", err));
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on ${port}...`));
